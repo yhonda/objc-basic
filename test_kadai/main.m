52023691019cccf383ac711ef8e60bc2807d3d30
Dai Kawahara <skbsporta7@gmail.com>
2017-08-05 22:50:43 +0900

    Initial Commit

---
+++ test_kadai/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test_kadai
+//
+//  Created by kawaharadai on 2017/08/05.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
