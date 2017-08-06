ddeca6345601c242505d0d2bdd73d52db197427d
Dai Kawahara <skbsporta7@gmail.com>
2017-08-06 23:22:43 +0900

    Fixed. #1の課題が完了しました。

--- test_kadai/main.m
+++ test_kadai/main.m
@@ -1,17 +1,25 @@
-//
-//  main.m
-//  test_kadai
-//
-//  Created by kawaharadai on 2017/08/05.
-//  Copyright © 2017年 dai kawahara. All rights reserved.
-//
+
 
 #import <Foundation/Foundation.h>
 
+//BOOL型、NSString型、NSInteger型、NSNumber型(初期値設定はできない)
+BOOL test1 = YES;
+NSString *test2 = @"mojiretsu";
+NSInteger test3 = 10;
+NSNumber *test4;
+
+
+//出力メソッド
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //値を設定
+        test4 = [NSNumber numberWithInt:10];
+        
+        //出力
+        NSLog(@"%hhd", test1);
+        NSLog(@"%@", test2);
+        NSLog(@"%ld", (long)test3);
+        NSLog(@"%@", test4);
     }
-    return 0;
+   
 }
