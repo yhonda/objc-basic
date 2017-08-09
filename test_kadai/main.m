589f05e1c3d3d7af302c73d440b4e2481f9c64cb
Dai Kawahara <skbsporta7@gmail.com>
2017-08-10 00:23:34 +0900

    Fiexd. #1の課題の修正が完了しました。

--- test_kadai/main.m
+++ test_kadai/main.m
@@ -1,25 +1,29 @@
+//
+//  main.m
+//  test_kadai
+//
+//  Created by kawaharadai on 2017/08/05.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
 
 
 #import <Foundation/Foundation.h>
 
-//BOOL型、NSString型、NSInteger型、NSNumber型(初期値設定はできない)
-BOOL test1 = YES;
-NSString *test2 = @"mojiretsu";
-NSInteger test3 = 10;
-NSNumber *test4;
-
-
 //出力メソッド
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        //値を設定
-        test4 = [NSNumber numberWithInt:10];
         
+        //BOOL型、NSString型、NSInteger型、NSNumber型(Int)の変数をそれぞれ定義
+        BOOL Bool_value = YES;
+        NSString *NSstring_value = @"mojiretsu";
+        NSInteger NSInteger_value = 10;
+        NSNumber *NSNumber_value = @10;
+
         //出力
-        NSLog(@"%hhd", test1);
-        NSLog(@"%@", test2);
-        NSLog(@"%ld", (long)test3);
-        NSLog(@"%@", test4);
+        NSLog(@"%hhd", Bool_value);
+        NSLog(@"%@", NSstring_value);
+        NSLog(@"%zd", NSInteger_value);
+        NSLog(@"%@", NSNumber_value);
     }
-   
+   return 0;
 }
