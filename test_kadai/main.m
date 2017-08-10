17f8a059a157269c33dac61ce2a61f5ba7f48d4e
Dai Kawahara <skbsporta7@gmail.com>
2017-08-11 02:41:19 +0900

    Fiexd. #1の課題の修正が完了しました。

--- test_kadai/main.m
+++ test_kadai/main.m
@@ -6,24 +6,21 @@
 //  Copyright © 2017年 dai kawahara. All rights reserved.
 //
 
-
 #import <Foundation/Foundation.h>
 
-//出力メソッド
+// 出力メソッド
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        //BOOL型、NSString型、NSInteger型、NSNumber型(Int)の変数をそれぞれ定義
-        BOOL Bool_value = YES;
-        NSString *NSstring_value = @"mojiretsu";
-        NSInteger NSInteger_value = 10;
-        NSNumber *NSNumber_value = @10;
-
-        //出力
-        NSLog(@"%hhd", Bool_value);
-        NSLog(@"%@", NSstring_value);
-        NSLog(@"%zd", NSInteger_value);
-        NSLog(@"%@", NSNumber_value);
+        // BOOL型、NSString型、NSInteger型、NSNumber型(Int)の変数をそれぞれ定義
+        BOOL boolStatus = YES;
+        NSString *string = @"mojiretsu";
+        NSInteger integer = 10;
+        NSNumber *number = @10;
+        // 出力
+        NSLog(@"%hhd", boolStatus);
+        NSLog(@"%@", string);
+        NSLog(@"%zd", integer);
+        NSLog(@"%@", number);
     }
    return 0;
 }
