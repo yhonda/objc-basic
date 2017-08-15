580193e103421d59a24628c26fa822594cdd3120
Dai Kawahara <skbsporta7@gmail.com>
2017-08-16 01:42:38 +0900

    Fiexd. #2の課題の修正が完了しました。 レビューにてご指摘頂いた箇所を修正致しました。

--- STVProject1-1-2/STVProject1-1-2/main.m
+++ STVProject1-1-2/STVProject1-1-2/main.m
@@ -13,22 +13,17 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // NSArray型、NSDictonary型の変数を定義
-        NSArray *fruitsArray;
-        NSDictionary *fruitsColorArray;
-        // 値を設定
-        fruitsArray = [NSArray arrayWithObjects:@"りんご",@"バナナ",@"ぶどう",nil];
-        fruitsColorArray = [NSDictionary dictionaryWithObjectsAndKeys:
-                            @"赤色", @"りんご",
-                            @"黄色", @"バナナ",
-                            @"紫色", @"ぶどう",nil];
+        NSArray *fruitsArray = @[@"りんご",@"バナナ",@"ぶどう"];
+        NSDictionary *fruitsColorArray = @{@"りんご":@"赤色",@"バナナ":@"黄色",@"ぶどう":@"紫色"};
+
         // NSArray型変数の出力
         for(int i=0; i<fruitsArray.count; i++){
-            NSLog(@"%@", fruitsArray[i]);
+            NSLog(@"%d番目の要素は「%@」です。",i,fruitsArray[i]);
         }
         // NSDictonary型変数の出力
-        NSLog(@"%@", [fruitsColorArray objectForKey:@"りんご"]);
-        NSLog(@"%@", [fruitsColorArray objectForKey:@"バナナ"]);
-        NSLog(@"%@", [fruitsColorArray objectForKey:@"ぶどう"]);
+        NSLog(@"キー値「りんご」のとき、値「%@」",fruitsColorArray[@"りんご"]);
+        NSLog(@"キー値「バナナ」のとき、値「%@」",fruitsColorArray[@"バナナ"]);
+        NSLog(@"キー値「ぶどう」のとき、値「%@」",fruitsColorArray[@"ぶどう"]);
     }
     return 0;
 }
