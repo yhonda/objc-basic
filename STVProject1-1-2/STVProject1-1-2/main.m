b4093b406e80b6f6c5edc0dcab7415cea4999c9c
Dai Kawahara <skbsporta7@gmail.com>
2017-08-18 05:11:37 +0900

    Fixed. #2の課題の修正が完了しました。 レビューにてご指摘頂いた箇所を修正致しました。

--- STVProject1-1-2/STVProject1-1-2/main.m
+++ STVProject1-1-2/STVProject1-1-2/main.m
@@ -14,16 +14,16 @@ int main(int argc, const char * argv[]) {
         
         // NSArray型、NSDictonary型の変数を定義
         NSArray *fruitsArray = @[@"りんご",@"バナナ",@"ぶどう"];
-        NSDictionary *fruitsColorArray = @{@"りんご":@"赤色",@"バナナ":@"黄色",@"ぶどう":@"紫色"};
+        NSDictionary *fruitsColorDictionary = @{@"りんご":@"赤色",@"バナナ":@"黄色",@"ぶどう":@"紫色"};
 
         // NSArray型変数の出力
         for(int i=0; i<fruitsArray.count; i++){
             NSLog(@"%d番目の要素は「%@」です。",i,fruitsArray[i]);
         }
         // NSDictonary型変数の出力
-        NSLog(@"キー値「りんご」のとき、値「%@」",fruitsColorArray[@"りんご"]);
-        NSLog(@"キー値「バナナ」のとき、値「%@」",fruitsColorArray[@"バナナ"]);
-        NSLog(@"キー値「ぶどう」のとき、値「%@」",fruitsColorArray[@"ぶどう"]);
+        NSLog(@"キー値「りんご」のとき、値「%@」",fruitsColorDictionary[@"りんご"]);
+        NSLog(@"キー値「バナナ」のとき、値「%@」",fruitsColorDictionary[@"バナナ"]);
+        NSLog(@"キー値「ぶどう」のとき、値「%@」",fruitsColorDictionary[@"ぶどう"]);
     }
     return 0;
 }
