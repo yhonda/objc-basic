9aadeedc5748f66f39b1e90a16008121dadba6c6
Dai Kawahara <skbsporta7@gmail.com>
2017-08-19 19:41:09 +0900

    Fiexd. #3の課題の修正が完了しました。 レビュー頂いた箇所の修正を致しました。

--- STVProject1-1-3/STVProject1-1-3/main.m
+++ STVProject1-1-3/STVProject1-1-3/main.m
@@ -16,21 +16,21 @@ int main(int argc, const char * argv[]) {
         NSInteger mathTestScore = 65;
         NSInteger chemicalTestScore = 40;
         NSInteger plusCount = 0;
-        NSArray *numberArray = @[@1,@2,@3,@4,@5,@6,@7,@8,@9];
-        NSInteger MethodNumber = 2;
+        NSArray *numberArray = @[@1, @2, @3, @4, @5, @6, @7, @8, @9];
         
         // 使用する定数の用意
-        static NSInteger *const PassTestScore = 60;
-        static NSInteger *const RepeatTestScore = 30;
-        typedef NS_ENUM(NSInteger, MethodNumberEnum) {
-            MethodNumber0 = 0,
-            MethodNumber1 = 1,
-            MethodNumber2 = 2,
+        static const NSInteger PassTestScore = 60;
+        static const NSInteger RepeatTestScore = 30;
+        typedef NS_ENUM(NSInteger, SignalAlertEnum) {
+            GoSignalAlert,
+            AttentionSignalAlert,
+            StopSignalAlert,
+            SignalAlertNumber = 2
         };
 
         // if文
         // 英語の試験結果(englishTestScore)、60点以上なら合格、59点以下なら不合格
-        if (englishTestScore >= (long)PassTestScore) {
+        if (englishTestScore >= PassTestScore) {
             NSLog(@"if文処理結果：英語の試験に”合格”しました。");
         } else {
             NSLog(@"if文処理結果：英語の試験は”不合格”でした。");
@@ -38,9 +38,9 @@ int main(int argc, const char * argv[]) {
         
         // if〜else文
         // 数学の試験結果(mathTestScore)、60点以上なら合格、59点〜30点なら再試験、29点以下なら不合格
-        if (mathTestScore >= (long) PassTestScore) {
+        if (mathTestScore >= PassTestScore) {
             NSLog(@"if〜else文処理結果：数学の試験に”合格”しました。");
-        } else if (mathTestScore >= (long) RepeatTestScore) {
+        } else if (mathTestScore >= RepeatTestScore) {
             NSLog(@"if〜else文処理結果：数学の試験は”再試験”です。");
         } else {
             NSLog(@"if〜else文処理結果：数学の試験は”不合格”です。");
@@ -48,8 +48,8 @@ int main(int argc, const char * argv[]) {
         
         // if文、三項演算子
         // 化学の試験結果(chemicalTestScore)、60点以上なら合格、59点以下なら不合格
-        NSString *result = (chemicalTestScore >= (long) PassTestScore) ? @"化学の試験に”合格”しました。" : @"化学の試験は”不合格”です。";
-        NSLog(@"if文、三項演算子文処理結果：%@",result);
+        NSString *result = (chemicalTestScore >= PassTestScore) ? @"化学の試験に”合格”しました。" : @"化学の試験は”不合格”です。";
+        NSLog(@"if文、三項演算子文処理結果：%@", result);
         
         // for文
         // 10回連続して「変数plusCountを2ずつ増やす」処理を行い、最後に処理結果を出力する。
@@ -65,19 +65,19 @@ int main(int argc, const char * argv[]) {
         }
         
         // switch文
-        // MethodNumberの値によって、メソッドを実行。
-        switch (MethodNumber) {
-            case MethodNumber0:
-                NSLog(@"switch文処理結果：0番のメソッドを実行します。");
+        // SignalAlertNumberの値によって、それぞれの指示を表示。
+        switch (SignalAlertNumber) {
+            case GoSignalAlert:
+                NSLog(@"switch文処理結果：進んでください。");
                 break;
-            case MethodNumber1:
-                NSLog(@"switch文処理結果：1番のメソッドを実行します。");
+            case AttentionSignalAlert:
+                NSLog(@"switch文処理結果：注意して進んでください。");
                 break;
-            case MethodNumber2:
-                NSLog(@"switch文処理結果：2番のメソッドを実行します。");
+            case StopSignalAlert:
+                NSLog(@"switch文処理結果：止まってください。");
                 break;
             default:
-                NSLog(@"switch文処理結果：実行するメソッドがありません。");
+                NSLog(@"switch文処理結果：問題発生、その場から動かないでください。");
                 break;
         }
     }
