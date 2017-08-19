86cb25656adb5e53a3425bb05bf9249d22d32bc9
Dai Kawahara <skbsporta7@gmail.com>
2017-08-20 01:14:15 +0900

    Fiexd. #7の課題が完了しました。

---
+++ STVProject2-1-2/STVProject2-1-2/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  STVProject2-1-2
+//
+//  Created by kawaharadai on 2017/08/16.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+// ストーリーボードのimageViewを定義
+@property (weak, nonatomic) IBOutlet UIImageView *foodImage;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 任意の画像をfoodImageにセットする
+    // アスペクト比の設定はストーリボード側にて実装(AspectFit)
+    _foodImage.image = [UIImage imageNamed:@"AustrariaBeef"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+@end
