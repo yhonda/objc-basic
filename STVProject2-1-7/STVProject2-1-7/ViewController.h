99ae6ce817b030cd55708349b7a568c53dc9ce99
Dai Kawahara <skbsporta7@gmail.com>
2017-08-23 21:22:17 +0900

    Fiexd. #12の課題が完了しました。

---
+++ STVProject2-1-7/STVProject2-1-7/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  STVProject2-1-7
+//
+//  Created by kawaharadai on 2017/08/23.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// デリゲートを接続
+@interface ViewController : UIViewController<UITextFieldDelegate>
+// プロパティを定義
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+@end
+
---
+++ STVProject2-1-7/STVProject2-1-7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  STVProject2-1-7
+//
+//  Created by kawaharadai on 2017/08/23.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
