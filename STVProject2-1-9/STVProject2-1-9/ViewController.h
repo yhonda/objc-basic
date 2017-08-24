343fa2f00a3022ec6bcacfea795c84d34ebde224
Dai Kawahara <skbsporta7@gmail.com>
2017-08-24 15:43:01 +0900

    Fiexd. #14の課題が完了しました。

---
+++ STVProject2-1-9/STVProject2-1-9/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  STVProject2-1-9
+//
+//  Created by kawaharadai on 2017/08/24.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// デリゲートを接続
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ STVProject2-1-9/STVProject2-1-9/ViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  STVProject2-1-9
+//
+//  Created by kawaharadai on 2017/08/24.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+// プロパティを宣言
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *pickerHiddenButton;
+@property NSDate *todayDate;
+@property NSDateFormatter *formatter;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 当日の日付を取得し、datapickerに表示する
+        // 当日の日付を取得
+    self.todayDate = [NSDate date];
+        // 表示用データフォーマットをインスタンス化
+    self.formatter = [[NSDateFormatter alloc] init];
+        // フォーマットに型をセット(時間まで表示する時は、@"M月dd日(E) HH:mm"でModeを変更する)
+    [self.formatter setDateFormat:@"yyyy年M月dd日"];
+        // ラベルに当日の日付を反映
+    self.dateLabel.text = [self.formatter stringFromDate:self.todayDate];
+    
+    // dateLabelにタグをつける（タッチイベント取得用）
+    self.dateLabel.tag = 1;
+    // dateLabelのタッチ判定を有効にする
+    self.dateLabel.userInteractionEnabled = YES;
+    // datePickerとdoneボタンを隠す
+    self.datePicker.hidden = YES;
+    self.pickerHiddenButton.hidden = YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+// ラベルタッチイベントの取得(ラベルタッチでpicker表示、背景タッチでpkicker非表示)
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // 受け取ったタッチイベントをインスタンス化
+    UITouch *touch = [[event allTouches] anyObject];
+    // タッチ箇所によってボタンとピッカーの表示/非表示を切り替える
+    if (touch.view.tag == self.dateLabel.tag){
+        self.datePicker.hidden = NO;
+        self.pickerHiddenButton.hidden = NO;
+    }else{
+        self.datePicker.hidden = YES;
+        self.pickerHiddenButton.hidden = YES;
+    }
+}
+
+- (IBAction)dateChangedAction:(id)sender {
+    // datapikerが変更されるたびにその日付を取得してpickerに入れる
+    self.dateLabel.text = [self.formatter stringFromDate:self.datePicker.date];
+}
+ // Doneボタンアクション
+- (IBAction)pickerHiddenAction:(id)sender {
+    self.datePicker.hidden = YES;
+    self.pickerHiddenButton.hidden = YES;
+}
+
+
+@end
