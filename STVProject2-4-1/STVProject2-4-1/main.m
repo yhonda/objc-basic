a6f6c8c5842cd62575c3c4f2199b509c93a11204
Dai Kawahara <skbsporta7@gmail.com>
2017-09-05 02:01:04 +0900

    Fiexd. #65の課題を完了しました。

---
+++ STVProject2-4-1/STVProject2-4-1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  STVProject2-4-1
+//
+//  Created by kawaharadai on 2017/09/04.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
