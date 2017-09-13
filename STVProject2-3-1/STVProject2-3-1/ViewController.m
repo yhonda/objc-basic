d791d6641e15a829f4dd1501f2e84e343f2f948b
Dai Kawahara <skbsporta7@gmail.com>
2017-09-14 01:29:50 +0900

    Fixed. #57 の課題の修正が完了しました。 下記の内容を修正しました。 ・公開不要なプロパティ、メソッドの削除 ・ユーザーに見える文字列はstringファイルで定義する ・データにアクセスするkeyを定数で定義する ・userDefaultsに値をセットした場合、その後synchronize処理を行うような変更 ・モダン記法への修正 ・プロパティには.でアクセスする

--- STVProject2-3-1/STVProject2-3-1/ViewController.m
+++ STVProject2-3-1/STVProject2-3-1/ViewController.m
@@ -14,8 +14,16 @@
 // メソッドを定義
 - (void)createNewData;
 - (BOOL)CheckRunfirstTime;
-- (IBAction)outputSaveDataButton:(id)sender;
 @end
+
+// 必要なKeyを定数で定義
+static NSString *const StringFileName = @"Localizable";
+static NSString *const RunFirstTimeKey = @"runFirstTime";
+static NSString *const NotRunFirstTimeKey = @"notRunFirstTime";
+static NSString *const CheckFirstRunTimeKey = @"firstRun";
+static NSString *const IntTypeValueKey = @"ValueTypeInt";
+static NSString *const DoubleTypeValueKey = @"ValueTypeDouble";
+static NSString *const StringTypeValueKey = @"ValueTypeString";
 
 @implementation ViewController
 
@@ -25,10 +33,10 @@
     // 初回起動を確認し、初回起動でなければ、それぞれの型で値を設定
     if ([self CheckRunfirstTime]) {
         NSLog(@"初回起動です。");
-        self.resultLabel.text = @"初回起動です。";
+        self.resultLabel.text = [NSBundle.mainBundle localizedStringForKey:RunFirstTimeKey value:nil table:StringFileName];
     } else {
         NSLog(@"初回起動ではありません。");
-        self.resultLabel.text = @"初回起動ではありません。";
+        self.resultLabel.text = [NSBundle.mainBundle localizedStringForKey:NotRunFirstTimeKey value:nil table:StringFileName];
         [self createNewData];
     }
 }
@@ -36,14 +44,14 @@
 // 初回起動を確認するメソッド
 - (BOOL)CheckRunfirstTime {
     //UserDefaultsに接続
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSUserDefaults *userDefaults = NSUserDefaults.standardUserDefaults;
     // 起動確認用の値が設定されていれば、NOを返し、されていなければ、起動証明として値を設定
-    if ([userDefaults objectForKey:@"firstRun"]) {
+    if ([userDefaults objectForKey:CheckFirstRunTimeKey]) {
         return NO;
     }
     // BoolをYESで保存
-    [userDefaults setBool:YES forKey:@"firstRun"];
-    // 即時反映
+    [userDefaults setBool:YES forKey:CheckFirstRunTimeKey];
+    // 更新をかける
     [userDefaults synchronize];
     // 初回起動
     return YES;
@@ -52,28 +60,30 @@
 // UserDefaultsに新たなデータを作成、保存する
 - (void)createNewData {
     //UserDefaultsに接続
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSUserDefaults *userDefaults = NSUserDefaults.standardUserDefaults;
     // Int型の値を保存
-    [userDefaults setInteger:1000 forKey:@"ValueTypeInt"];
+    [userDefaults setInteger:1000 forKey:IntTypeValueKey];
     // Double型の値を保存
-    [userDefaults setDouble:0.005 forKey:@"ValueTypeDouble"];
+    [userDefaults setDouble:0.005 forKey:DoubleTypeValueKey];
     // String型の値を保存
-    [userDefaults setObject:@"アイウエオ" forKey:@"ValueTypeString"];
+    [userDefaults setObject:@"アイウエオ" forKey:StringTypeValueKey];
+    // 更新をかける
+    [userDefaults synchronize];
 }
 
 // UserDefaultsに保存している値を出力する
 - (IBAction)outputSaveDataButton:(id)sender {
     
     //UserDefaultsに接続
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSUserDefaults *userDefaults = NSUserDefaults.standardUserDefaults;
     
     // 取り出した値をインスタンス化
-    int saveIntValue = (int)[userDefaults integerForKey:@"ValueTypeInt"];
-    double saveDoubleValue = [userDefaults doubleForKey:@"ValueTypeDouble"];
-    NSString *saveStringValue = [userDefaults stringForKey:@"ValueTypeString"];
+    int saveIntValue = (int)[userDefaults integerForKey:IntTypeValueKey];
+    double saveDoubleValue = [userDefaults doubleForKey:DoubleTypeValueKey];
+    NSString *saveStringValue = [userDefaults stringForKey:StringTypeValueKey];
     
     // 初回起動時の場合は、保存データがないことを通知して処理を終了させる
-    if (saveStringValue == nil) {
+    if (!saveIntValue && !saveDoubleValue && !saveStringValue) {
         return NSLog(@"保存されているデータはありません。");
     }
     
