39389a335a5427ce1ed7598b0dbcb3d08c4ff2ab
Dai Kawahara <skbsporta7@gmail.com>
2017-09-18 19:58:45 +0900

    Fixed. #78の課題が完了しました。

---
+++ STVProject2-5-6/STVProject2-5-6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STVProject2-5-6
+//
+//  Created by kawaharadai on 2017/09/18.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import  <CoreLocation/CoreLocation.h>
+
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+
+@end
+
