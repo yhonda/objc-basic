46dde44842a475716091eb46d3fd2dbc96a5ef3c
Dai Kawahara <skbsporta7@gmail.com>
2017-09-21 17:34:52 +0900

    Fixed. #83の課題が完了しました。

---
+++ STVProject2-6-2/STVProject2-6-2/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  STVProject2-6-2
+//
+//  Created by kawaharadai on 2017/09/21.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+@property (strong, nonatomic) NSString *receiveUrl;
+@property (strong, nonatomic) NSString *receiveScheme;
+@property (strong, nonatomic) NSString *receiveQuery;
+@end
---
+++ STVProject2-6-2/STVProject2-6-2/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  STVProject2-6-2
+//
+//  Created by kawaharadai on 2017/09/21.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
