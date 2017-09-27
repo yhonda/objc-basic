ed46cbf005fc14fd86fa000395b712b21806e40e
Dai Kawahara <skbsporta7@gmail.com>
2017-09-27 13:41:40 +0900

    UICollectionViewにて本年度のカレンダーの一覧を作成 カレンダー初期表示を現在の月にて表示

--- CalendarApp/CalendarApp/Controllers/ViewController.m
+++ CalendarApp/CalendarApp/Controllers/ViewController.m
@@ -7,24 +7,146 @@
 //
 
 #import "ViewController.h"
+#import "CustomCollectionViewCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDelegate,UICollectionViewDataSource>
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
-
+@property (strong, nonatomic) NSDate *daysData;
 @end
+
+// 週間の日数
+static int const DaysWeek = 7;
+// セル間のマージン
+static CGFloat const CellMargin = 2.0f;
 
 @implementation ViewController
 
+#pragma mark - LifeCycle methods
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // 当日の日付を取得（これを基準に表示月を決定）
+    self.daysData = [NSDate date];
+    // カスタムセルnibの登録
+    UINib *nib = [UINib nibWithNibName:@"CustomCollectionViewCell" bundle:nil];
+    [self.collectionView registerNib:nib forCellWithReuseIdentifier:@"Cell"];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Action methods
+#pragma mark - private methods
+// 本日の日付を出すアクセスするごとにindePathによって、初日から絡んだーを進めていく
+- (NSDate *)getTargetDate:(NSIndexPath *)indexPath {
+    // その月の初めの日を返す
+    NSInteger ordinalityOfFirstDay = [NSCalendar.currentCalendar ordinalityOfUnit:NSCalendarUnitDay
+                                                                           inUnit:NSCalendarUnitWeekOfMonth
+                                                                          forDate:[self firstDayofMonth]];
+    // データコンポーネントを初期化して用意
+    NSDateComponents *dateComponents = [[NSDateComponents alloc]init] ;
+    // 先月の月末の日数を出している
+    dateComponents.day = indexPath.item - (ordinalityOfFirstDay - 1);
+    //    NSLog(@"もしかすると先月の日付数%ld", (long)dateComponents.day);
+    
+    // 本日の日付を出す
+    NSDate *date = [NSCalendar.currentCalendar dateByAddingComponents:dateComponents
+                                                               toDate:[self firstDayofMonth]
+                                                              options:0];
+    return date;
+}
+// 先月最後の日付を出すメソッド
+- (NSDate *)firstDayofMonth {
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay
+                                                                   fromDate:self.daysData];
+    // 当月の日を1日に戻す
+    components.day = 1;
+    // 当日の日付を返す
+    NSDate *firstDateMonth = [[NSCalendar currentCalendar] dateFromComponents:components];
+    
+    return firstDateMonth;
+}
+
+#pragma mark - UICollectionViewDataSource methods
+// セクションの数
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 2;
+}
+// セッションの内容
+- (UICollectionReusableView*)collectionView:(UICollectionView *)collectionView
+          viewForSupplementaryElementOfKind:(NSString *)kind
+                                atIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *header = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"Header" forIndexPath:indexPath];
+    return header;
+}
+// セルの数
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section {
+    
+    if (section == 0) {
+        return 7;
+    } else {
+        // 当日が含まれている月の週の数を数える
+        NSRange rangeOfWeeks = [NSCalendar.currentCalendar rangeOfUnit:NSCalendarUnitWeekOfMonth
+                                                                inUnit:NSCalendarUnitMonth
+                                                               forDate:[self daysData]];
+        // その月が何週間あるかを数える
+        NSUInteger numberOfWeeks = rangeOfWeeks.length;
+        // 週間＊7日で月の全日数を計算
+        NSInteger numberOfItems = numberOfWeeks * DaysWeek;
+        // 日数を返す
+        return numberOfItems;
+    }
+}
+
+
+
+// セルの内容
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CustomCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell"
+                                                                               forIndexPath:indexPath];
+    
+    cell.cellLabel.textColor = [UIColor blackColor];
+    
+        // 日付のフォーマットを作成
+        NSDateFormatter *formatter = [NSDateFormatter new];
+        // daysのみを指定
+        formatter.dateFormat = @"d";
+        // セットした日付からindex順に取り出す
+        cell.cellLabel.text = [formatter stringFromDate:[self getTargetDate:indexPath]];
+    
+    return cell;
+}
+
+
+
+
+#pragma mark - UICollectionViewDelegate methods
+// セル間4方向のマージンを決定するメソッド
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsMake(CellMargin, CellMargin, CellMargin, CellMargin);
+}
+
+// セルの隙間を埋める
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return CellMargin;
+}
+
+//
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return CellMargin;
+}
+
+
+
+// セクションのサイズを設定
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
+    CGFloat cellHeight = 0;
+    CGFloat sectionWidth = self.collectionView.bounds.size.width;
+    CGSize sectionSize = CGSizeMake(sectionWidth, cellHeight);
+    return sectionSize;
+}
 
 @end
