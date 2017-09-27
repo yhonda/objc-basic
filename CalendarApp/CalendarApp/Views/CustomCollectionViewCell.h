702bd351075bdd528a3aebe3810591f5a903ca6b
Dai Kawahara <skbsporta7@gmail.com>
2017-09-27 13:16:43 +0900

    ストーリーボード側のレイアウト完了

---
+++ CalendarApp/CalendarApp/Views/CustomCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.h
+//  CalendarApp
+//
+//  Created by kawaharadai on 2017/09/27.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *cellLabel;
+@end
---
+++ CalendarApp/CalendarApp/Views/CustomCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.m
+//  CalendarApp
+//
+//  Created by kawaharadai on 2017/09/27.
+//  Copyright © 2017年 dai kawahara. All rights reserved.
+//
+
+#import "CustomCollectionViewCell.h"
+
+@implementation CustomCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
